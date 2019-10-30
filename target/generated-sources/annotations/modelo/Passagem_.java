a3374c07b498f33bef1828f06d37d8c4db80c1e8
Pedro Portes <pedrohm.portes@gmail.com>
2019-10-29 22:00:20 -0300

    teste

---
+++ target/generated-sources/annotations/modelo/Passagem_.java
@@ -0,0 +1,29 @@
+package modelo;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-10-29T18:44:06")
+@StaticMetamodel(Passagem.class)
+public class Passagem_ { 
+
+    public static volatile SingularAttribute<Passagem, String> paisDestinoVolta;
+    public static volatile SingularAttribute<Passagem, String> paisOrigemIda;
+    public static volatile SingularAttribute<Passagem, String> ufOrigemVolta;
+    public static volatile SingularAttribute<Passagem, String> paisDestinoIda;
+    public static volatile SingularAttribute<Passagem, String> cidadeDestinoVolta;
+    public static volatile SingularAttribute<Passagem, Integer> identificadorProcessoViagem;
+    public static volatile SingularAttribute<Passagem, String> ufOrigemIda;
+    public static volatile SingularAttribute<Passagem, String> meioTransporte;
+    public static volatile SingularAttribute<Passagem, String> cidadeOrigemIda;
+    public static volatile SingularAttribute<Passagem, String> cidadeOrigemVolta;
+    public static volatile SingularAttribute<Passagem, String> ufDestinoIda;
+    public static volatile SingularAttribute<Passagem, String> paisOrigemVolta;
+    public static volatile SingularAttribute<Passagem, String> ufDestinoVolta;
+    public static volatile SingularAttribute<Passagem, Float> valorPassagem;
+    public static volatile SingularAttribute<Passagem, Integer> id;
+    public static volatile SingularAttribute<Passagem, String> cidadeDestinoIda;
+    public static volatile SingularAttribute<Passagem, Float> taxaServico;
+
+}
---
+++ target/generated-sources/annotations/modelo/Trecho_.java
@@ -0,0 +1,26 @@
+package modelo;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-10-29T18:44:06")
+@StaticMetamodel(Trecho.class)
+public class Trecho_ { 
+
+    public static volatile SingularAttribute<Trecho, String> origemCidade;
+    public static volatile SingularAttribute<Trecho, Integer> sequenciaTrecho;
+    public static volatile SingularAttribute<Trecho, String> destinoData;
+    public static volatile SingularAttribute<Trecho, String> origemPais;
+    public static volatile SingularAttribute<Trecho, String> origemUf;
+    public static volatile SingularAttribute<Trecho, Integer> identificadorProcessoViagem;
+    public static volatile SingularAttribute<Trecho, String> meioTransporte;
+    public static volatile SingularAttribute<Trecho, String> destinoUf;
+    public static volatile SingularAttribute<Trecho, String> destinoCidade;
+    public static volatile SingularAttribute<Trecho, Boolean> missao;
+    public static volatile SingularAttribute<Trecho, String> destinoPais;
+    public static volatile SingularAttribute<Trecho, Float> numeroDiarias;
+    public static volatile SingularAttribute<Trecho, Integer> id;
+    public static volatile SingularAttribute<Trecho, String> origemData;
+
+}
